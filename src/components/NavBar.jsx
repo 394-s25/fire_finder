577db127ea9aa1fc5d899554de0574548f956b87
Hayah Ubaid <hayah.ubaid@gmail.com>
2025-05-14 10:50:01 -0500

    Logo and bg color changed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,7 @@ import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 import LocalActivityOutlinedIcon from '@mui/icons-material/LocalActivityOutlined';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import { useNavigate, useLocation } from 'react-router-dom';
+import Flogo from "../imgs/Flogo.png"; 
 
 const Navbar = () => {
   const [isPanelOpen, setIsPanelOpen] = useState(false);
@@ -34,11 +35,21 @@ const Navbar = () => {
     <>
       <AppBar
         position="fixed"
-        sx={{ backgroundColor: 'rgb(255, 251, 251)', boxShadow: '0 2px 4px rgba(0,0,0,0.2)', zIndex: 1100 }}
+        sx={{
+          backgroundColor: "rgb(255, 251, 251)",
+          boxShadow: "0 2px 4px rgba(0,0,0,0.2)",
+          zIndex: 1100,
+        }}
       >
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-middle' }}>
-            <Typography
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-middle",
+            }}
+          >
+            {/* <Typography
               variant="h8"
               sx={{
                 color: '#DC2626',
@@ -49,27 +60,28 @@ const Navbar = () => {
               }}
             >
               FireFinder
-            </Typography>
+            </Typography> */}
             <Box
               component="img"
-              src={logo}
+              src={Flogo}
               alt="TC logo"
               sx={{
-                width: { xs: '40px', sm: '50px', md: '45px' },
-                height: { xs: '52px', sm: '65px', md: '50px' },
-                marginLeft: { xs: '5px', sm: '10px', md: '10px' },
-                marginBottom: { xs: '2px', sm: '3px', md: '3px' },
+                width: { xs: "40px", sm: "50px", md: "120px" },
+                height: { xs: "52px", sm: "65px", md: "80px" },
+                marginLeft: { xs: "5px", sm: "10px", md: "-20px" },
+                marginTop: { xs: "2px", sm: "3px", md: "3px" },
+                marginBottom: { xs: "2px", sm: "3px", md: "3px" },
               }}
             />
           </div>
           <Typography
             variant="h2"
             sx={{
-              color: '#DC2626',
+              color: "#DC2626",
               fontFamily: '"Times New Roman", Georgia, serif',
               marginBottom: 1,
               marginTop: 1,
-              fontSize: { xs: '1.25rem', sm: '1.5rem', md: '2rem' },
+              fontSize: { xs: "1.25rem", sm: "1.5rem", md: "2rem" },
             }}
           >
             {currentTitle}
@@ -82,9 +94,9 @@ const Navbar = () => {
           >
             <MenuIcon
               sx={{
-                color: '#DC2626',
-                alignItems: 'middle',
-                fontSize: { xs: '20px', sm: '24px', md: '24px' },
+                color: "#DC2626",
+                alignItems: "middle",
+                fontSize: { xs: "20px", sm: "24px", md: "24px" },
               }}
             />
           </IconButton>
@@ -96,112 +108,116 @@ const Navbar = () => {
         onClose={togglePanel}
         sx={{
           zIndex: 1500,
-          '& .MuiDrawer-paper': {
-            width: { xs: '80px', sm: '100px', md: '100px' },
-            backgroundColor: 'white',
-            boxShadow: '0 0 10px rgba(0,0,0,0.1)',
-            marginTop: '100px',
-            borderRadius: '10px',
+          "& .MuiDrawer-paper": {
+            width: { xs: "80px", sm: "100px", md: "100px" },
+            backgroundColor: "white",
+            boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+            marginTop: "100px",
+            borderRadius: "10px",
           },
         }}
       >
-        <List sx={{ width: '100%' }}>
+        <List sx={{ width: "100%" }}>
           <ListItem
             button
-            onClick={() => handleNavigation('/profile')}
+            onClick={() => handleNavigation("/profile")}
             sx={{
-              flexDirection: 'column',
-              alignItems: 'center',
-              backgroundColor: location.pathname === '/profile' ? '#FEE2E2' : 'transparent',
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor:
+                location.pathname === "/profile" ? "#FEE2E2" : "transparent",
             }}
           >
             <>
               <AccountCircleIcon
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '20px', sm: '24px', md: '24px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "20px", sm: "24px", md: "24px" },
                 }}
               />
               <ListItemText
                 primary="Profile"
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '8px', sm: '10px', md: '10px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "8px", sm: "10px", md: "10px" },
                 }}
               />
             </>
           </ListItem>
           <ListItem
             button
-            onClick={() => handleNavigation('/resources')}
+            onClick={() => handleNavigation("/resources")}
             sx={{
-              flexDirection: 'column',
-              alignItems: 'center',
-              backgroundColor: location.pathname === '/resources' ? '#FEE2E2' : 'transparent',
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor:
+                location.pathname === "/resources" ? "#FEE2E2" : "transparent",
             }}
           >
             <>
               <GroupsOutlinedIcon
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '20px', sm: '24px', md: '24px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "20px", sm: "24px", md: "24px" },
                 }}
               />
               <ListItemText
                 primary="Resources"
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '8px', sm: '10px', md: '10px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "8px", sm: "10px", md: "10px" },
                 }}
               />
             </>
           </ListItem>
           <ListItem
             button
-            onClick={() => handleNavigation('/events')}
+            onClick={() => handleNavigation("/events")}
             sx={{
-              flexDirection: 'column',
-              alignItems: 'center',
-              backgroundColor: location.pathname === '/events' ? '#FEE2E2' : 'transparent',
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor:
+                location.pathname === "/events" ? "#FEE2E2" : "transparent",
             }}
           >
             <>
               <LocalActivityOutlinedIcon
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '20px', sm: '24px', md: '24px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "20px", sm: "24px", md: "24px" },
                 }}
               />
               <ListItemText
                 primary="Events"
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '8px', sm: '10px', md: '10px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "8px", sm: "10px", md: "10px" },
                 }}
               />
             </>
           </ListItem>
           <ListItem
             button
-            onClick={() => handleNavigation('/')}
+            onClick={() => handleNavigation("/")}
             sx={{
-              flexDirection: 'column',
-              alignItems: 'center',
-              backgroundColor: location.pathname === '/' ? '#FEE2E2' : 'transparent',
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor:
+                location.pathname === "/" ? "#FEE2E2" : "transparent",
             }}
           >
             <>
               <HomeOutlinedIcon
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '20px', sm: '24px', md: '24px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "20px", sm: "24px", md: "24px" },
                 }}
               />
               <ListItemText
                 primary="Explore"
                 sx={{
-                  color: '#DC2626',
-                  fontSize: { xs: '8px', sm: '10px', md: '10px' },
+                  color: "#DC2626",
+                  fontSize: { xs: "8px", sm: "10px", md: "10px" },
                 }}
               />
             </>
