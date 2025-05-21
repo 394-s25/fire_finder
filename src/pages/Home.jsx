de9bf79bbf286b151ad2969575200763a84d0bd4
Luke Huff <lukerhuff@gmail.com>
2025-05-21 14:19:44 -0500

    pull user info from firestore in home page profile card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Navbar from '../components/NavBar';
-import ProfileCard from '../components/ProfileCard';
-import NewPostModal from '../components/NewPost';
-import PostsTabs from '../components/PostsTabs';
-import SortDropdown from '../components/SortDropdown';
-import PostCard from '../components/PostCard';
-import SearchBar from '../components/HomeSearch'; 
+import React from "react";
+import Navbar from "../components/NavBar";
+import ProfileCard from "../components/ProfileCard";
+import NewPostModal from "../components/NewPost";
+import PostsTabs from "../components/PostsTabs";
+import SortDropdown from "../components/SortDropdown";
+import PostCard from "../components/PostCard";
+import SearchBar from "../components/HomeSearch";
 
 const Home = () => {
   return (
@@ -17,7 +17,6 @@ const Home = () => {
       <SearchBar />
       <SortDropdown />
       <PostCard />
-      {/* Add your main content here */}
     </>
   );
 };
