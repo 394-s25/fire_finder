ddd64bf28e4c45980b734f0e1b11d794edcc8cad
Luke Huff <lukerhuff@gmail.com>
2025-06-02 10:40:03 -0500

    broke unit test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -31,6 +31,6 @@ describe("Fire Finder Tests", () => {
         <App />
       </UserProvider>
     );
-    expect(screen.getByText("Explore")).toBeInTheDocument();
+    expect(screen.getByText("Explofoore")).toBeInTheDocument();
   });
 });
